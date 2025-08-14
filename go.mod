c647e1d7d07939d07758c611bd24e250477be053
bycait27 <caitlinba27@gmail.com>
2025-08-13 19:19:00 -0500

    Add base model structures for README generator with validation tags using validator/v10.

--- go.mod
+++ go.mod
@@ -5,11 +5,20 @@ go 1.24.1
 require (
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/fatih/color v1.18.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
+	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/manifoldco/promptui v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
---
+++ internal/models/base.go
@@ -0,0 +1,30 @@
+package models
+
+type BaseInfo struct {
+	Title       string       `validate:"required,min=1,max=100"`
+	Description string       `validate:"required,min=10,max=500"`
+	Screenshots *Screenshots `validate:"omitempty"` // optional
+	TechStack   *TechStack   `validate:"omitempty"` // optional for CLI tools
+	License     string       `validate:"required,oneof=MIT Apache-2.0 GPL-3.0 BSD-3-Clause ISC Unlicense"`
+	Author      AuthorInfo   `validate:"required"`
+}
+
+type Screenshots struct {
+	Demo        string   `validate:"required,filepath"`       // path to demo gif/video
+	Images      []string `validate:"omitempty,dive,filepath"` // optional additional screenshots
+	Description string   `validate:"required,min=5,max=200"`  // caption for demo
+}
+
+type TechStack struct {
+	Language     string   `validate:"required,min=1,max=50"`
+	Framework    string   `validate:"omitempty,max=50"`             // optional
+	Database     string   `validate:"omitempty,max=50"`             // optional
+	Dependencies []string `validate:"omitempty,dive,min=1,max=100"` // optional
+}
+
+type AuthorInfo struct {
+	Name    string `validate:"required,min=2,max=50"`
+	Email   string `validate:"required,email"`
+	GitHub  string `validate:"required,url"`  // GitHub profile url
+	Website string `validate:"omitempty,url"` // optional personal website
+}
